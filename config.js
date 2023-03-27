7251951b7ee73737e22a939c26cdd3c0c6d42ddf
Aleksejs, Levenciks [External] <Levenciks.Aleksejs@externals.adidas.com>
2023-03-28 01:24:22 +0300

    init commit

---
+++ config.js
@@ -0,0 +1,18 @@
+export const config = {
+    STG: {
+        host: 'https://gorest.co.in/public/v2',
+        token: 'e3800b50df1afd29775ee8d0bc53d1435a4cb5924dcd3a99ed6588f298b55aeb',
+        username: 'Tenali Ramakrishna',
+        gender: 'male',
+        status: 'active'
+    },
+    PROD: {
+        host: 'https://gorest.co.in/public/v2',
+        token: 'e3800b50df1afd29775ee8d0bc53d1435a4cb5924dcd3a99ed6588f298b55aeb',
+        username: 'Tenali Ramakrishna',
+        gender: 'male',
+        status: 'active'
+    }
+}
+
+global.executionVariables = {}
